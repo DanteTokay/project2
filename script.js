cf3e6cb91c18a3eb09573c3f9c62b783e0c1cee0
Dante Tokay <valley4stanlley@gmail.com>
2020-04-05 12:58:49 +0200

    if condition was corected

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const baton = document.querySelector('button');
 let numberLi =1;
 let nrToBold =1;
 const addLi = function(){
-  numberLi ++;
-  nrToBold ++;
+  numberLi +=2;
+ //  nrToBold ++;
   const newLi = document.createElement('li');
   newLi.textContent=numberLi;
 
-    if(nrToBold ==3){
+    //if(nrToBold ==3)
+    if(numberLi%3==0){
       newLi.classList.add('bold');
-        nrToBold=0;
+        //nrToBold=0;
     } ;
 
     document.querySelector('ul').appendChild(newLi);
